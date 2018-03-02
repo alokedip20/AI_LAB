8fa39a7c4543ba753ace30d15af9a65546a6e671
alokedip20 <alokengineer14@gmail.com>
2018-03-02 20:23:24 +0530

    modified

--- Assignment2/quick_sort_wa.pl
+++ Assignment2/quick_sort_wa.pl
@@ -1,14 +1,15 @@
-quicksort(L, S):-
-    quicksortacc(L, [], S).
-quicksortacc([], A, A).
-quicksortacc([X|L], A, S):-
-    partition(L, Lt, X, Gt),
-    quicksortacc(Gt, A, Gs),
-    quicksortacc(Lt, [X|Gs], S).
-partition([], [], _, []).
-partition([X|L], [X|Lt], H, Gt):-
-    X =< H,
-    !,
-    partition(L, Lt, H, Gt).
-partition([X|L], Lt, H, [X|Gt]):-
-	partition(L, Lt, H, Gt).
+quick_sort(List,Sorted_list):-
+	quick_sort_acc(List,[],Sorted_list).
+quick_sort_acc([],A,A).
+quick_sort_acc([Pivot|Rest],A,Sorted_list):-
+	partition(Rest,Left_half,Pivot,Right_half),
+	quick_sort_acc(Left_half,A,Left_sorted),
+	append(Left_sorted,[Pivot],Left_final),
+	quick_sort_acc(Right_half,Left_final,Sorted_list).
+partition([],[],_,[]).
+partition([Head|Rest],[Head|Left_half],Pivot,Right_half):-
+	Head =< Pivot,
+		partition(Rest,Left_half,Pivot,Right_half).
+partition([Head|Rest],Left_half,Pivot,[Head|Right_half]):-
+	Head > Pivot,
+		partition(Rest,Left_half,Pivot,Right_half).
--- Assignment2/quick_sort_woa.pl
+++ Assignment2/quick_sort_woa.pl
@@ -1,19 +1,16 @@
 quick_sort([],[]).
-quick_sort([Pivot|Rest],Result):-
-	partition(Rest,Pivot,Left,Right),
-	quick_sort(Left,Left_sort),
-	quick_sort(Right,Right_sort),
-	my_append(Left_sort,[Pivot|Right_sort],Result).
+quick_sort([Pivot|Rest],Sorted_list):-
+	partition(Rest,Left_half,Pivot,Right_half),
+	quick_sort(Left_half,Left_sorted),
+	append(Left_sorted,[Pivot],Left_final),
+	quick_sort(Right_half,Right_sorted),
+	append(Left_final,Right_sorted,Sorted_list).
 
-partition([],Pivot,[],[]).
-partition([Head1|L1],Pivot,[Head1|Left_sort],Right_sort):-
-	Head1 =< Pivot,
-		partition(L1,Pivot,Left_sort,Right_sort).
+partition([],[],_,[]).
+partition([Head|Rest],[Head|Left_half],Pivot,Right_half):-
+	Head =< Pivot,
+		partition(Rest,Left_half,Pivot,Right_half).
 
-partition([Head1|L1],Pivot,Left_sort,[Head1|Right_sort]):-
-        Head1 > Pivot,
-                partition(L1,Pivot,Left_sort,Right_sort).
-
-my_append([],List,List).
-my_append([Head1|List1],List2,[Head1|List3]):-
-	my_append(List1,List2,List3).
+partition([Head|Rest],Left_half,Pivot,[Head|Right_half]):-
+        Head > Pivot,
+                partition(Rest,Left_half,Pivot,Right_half).
