284d1b89b59ad72ebae7d71c123ebe39be149862
alokedip20 <alokengineer14@gmail.com>
2018-03-02 20:56:00 +0530

    modified

--- Assignment1/selection_sort.pl
+++ Assignment1/selection_sort.pl
@@ -1,11 +1,22 @@
 selection_sort([],[]).
-selection_sort(L,[H|T]):-
-	least(H,L,R),
-	selection_sort(R,T).
-least(H,L,R):-
-	my_select(H,L,R),
-	smaller(H,R).
-smaller(_,[]).
-smaller(X,[H|T]):-
-	X =< H,
-	smaller(X,T).
+selection_sort([X],[X]).
+selection_sort(List,[Head|Sorted_list]):-
+	find_minimum(List,Head),
+	my_select(List,Head,Rest),
+	selection_sort(Rest,Sorted_list).
+
+find_minimum([Head|List],Result):-
+	minimum_acc(List,Head,Result).
+minimum_acc([],A,A).
+minimum_acc([Head|Rest],A,Result):-
+	Head < A,
+		minimum_acc(Rest,Head,Result).
+minimum_acc([Head|Rest],A,Result):-
+        Head >= A,
+                minimum_acc(Rest,A,Result).
+
+my_select([],_,[]).
+my_select([X|Rest],X,Rest).
+my_select([X|Rest],Y,[X|Result]):-
+	my_select(Rest,Y,Result).
+
