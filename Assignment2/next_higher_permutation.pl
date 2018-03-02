49ac4ad3c2c16340c938ec1900b655a5fdfa171c
alokedip20 <alokengineer14@gmail.com>
2018-03-02 23:05:20 +0530

    added

--- Assignment2/next_higher_permutation.pl
+++ Assignment2/next_higher_permutation.pl
@@ -1,29 +1,54 @@
-my_select(X,[X|R],R).
-my_select(X,[Y|L],[Y|R]):-
-	my_select(X,L,R).
-permutation([],[]).
-permutation(L,[H|R]):-
-	my_select(H,L,T),
-	permutation(T,R).
+nhp(List,Result):-
+	digit(List,Initial),
+	nb_setval(diff,99999999),
+	nb_setval(no,-1),
+	not(nhp_acc(List,Initial,A,Result)),
+	nb_getval(no,X),
+	write("Digit ":X),
+	convert_list(X,Result).
 
-next_higher_permutation(List,Result):-
-	permutation(List,Result),
-	check_higher(List,Result),
+nhp_acc(List,Initial,A,Result):-
+	permutation(List,Temp),
+	digit(Temp,X),
+	T is X - Initial,
+	nb_getval(diff,Diff),
+	T > 0,
+		T < Diff,
+			nb_setval(diff,T),
+			nb_setval(no,X),
+			fail.
+	
+
+
+digit(List,R):-
 	my_length(List,Length),
-	factorial(Length,No),
-	check_all_permutation(List,Result,No).
-
-check_higher([Head1|Rest1],[Head2|Rest2]):-
-	Head2 > Head1,
+	digit_acc(List,Length,0,R),
 	!
 	.
-check_higher([Head|Rest1],[Head|Rest2]):-
-	check_higher(Rest1,Rest2).
+digit_acc(List,0,A,A).	
+digit_acc([Head|Rest],Length,A,R):-
+	T is Length - 1,	
+	power(10,T,X),
+	M is Head * X,
+	Temp is A + M,
+	N is Length - 1,
+	digit_acc(Rest,N,Temp,R).
+	
 
-check_all_permutation(_,_,0).
-check_all_permutation(List,X,No):-
-	permutation(List,Temp),
-	check_higher(List,Temp),
-	not(check_higher(X,Temp)),!,
-	N is No - 1,
-	check_all_permutation(List,X,N).
+power(X,Y,R):-
+	power_acc(X,Y,1,R).
+power_acc(X,0,A,A).
+power_acc(X,Y,A,R):-
+	T is A * X,
+	P is Y - 1,
+	power_acc(X,P,T,R).
+
+convert_list(Digit,Result):-
+	list_acc(Digit,[],Result).
+
+list_acc(0,A,A).
+list_acc(Digit,A,Result):-
+	X is mod(Digit,10),
+	T is div(Digit,10),
+	list_acc(T,[X|A],Result).
+	
