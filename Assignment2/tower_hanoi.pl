51041587bd72e9da756f6f56a8a431a58c3def1f
alokedip20 <alokengineer14@gmail.com>
2018-03-03 17:28:37 +0530

    .

--- Assignment2/tower_hanoi.pl
+++ Assignment2/tower_hanoi.pl
@@ -1,11 +1,8 @@
-move_disk(1,X,Y,_) :-  
-	write('Move the Top disk from '), 
-	write(X), 
-	write(' to '), 
-	write(Y),nl. 
-move_disk(N,X,Y,Z) :- 
-	N > 1, 
-		M is N-1, 
-		move_disk(M,X,Z,Y), 
-		move_disk(1,X,Y,_), 
-		move_disk(M,Z,Y,X).
+move_disk(1,Beg,Aux,End):-
+	write("Move Top of The Disk From ":Beg),
+	write(" To ":End),nl.
+move_disk(N,Beg,Aux,End):-
+	M is N - 1,
+	move_disk(M,Beg,End,Aux),
+	move_disk(1,Beg,Aux,End),
+	move_disk(M,Aux,Beg,End).
