54a0be9cf44c3b6ba3e40631a2179ed77e88c7bd
alokedip20 <alokengineer14@gmail.com>
2018-03-31 15:17:45 +0530

    modified

--- Assignment3/8puzzle.py
+++ Assignment3/8puzzle.py
@@ -1,15 +1,16 @@
 import sys
 goal_state = [1,2,3,8,0,4,7,6,5]
 class node:
-	def __init__(self, state,parent,move,depth,h_val):
+	def __init__(self, state,parent,move,depth,h_val,node_cost):
 		self.state = state	
 		self.parent = parent
 		self.move = move
 		self.depth = depth
 		self.heuristic_value = h_val
+		self.cost = node_cost
 
-def create_node(state,parent,move,depth,h_val = 0):
-	return node(state,parent,move,depth,h_val)
+def create_node(state,parent,move,depth,h_val = 0,node_cost = 0):
+	return node(state,parent,move,depth,h_val,node_cost)
 
 def generate_children(node,open_list,close_list):
 	children = []
@@ -18,9 +19,26 @@ def generate_children(node,open_list,close_list):
 	children.append(create_node(tile_left(node.state),node,'left',node.depth + 1,heuristic(tile_left(node.state),goal_state)))
 	children.append(create_node(tile_right(node.state),node,'right',node.depth + 1,heuristic(tile_right(node.state),goal_state)))
 	children = [child for child in children if child.state != None]
-	children = [child for child in children if child.state not in open_list]
-	children = [child for child in children if child.state not in close_list]
+	#children = [child for child in children if child.state not in open_list]
+	#children = [child for child in children if child.state not in close_list]
+	children = [child for child in children if not member_o(child,open_list)]
+	children = [child for child in children if not member_c(child,close_list)]
 	return children
+
+def member_o(i,open_list):
+	for index,elem in enumerate(open_list):
+		if i.state == elem.state:
+			print (str(index)+' element is in open list : '+str(i.state))
+			return True
+	return False
+
+def member_c(i,close_list,flag = 0):
+	for index,elem in enumerate(close_list):
+		if i.state == elem.state:
+			if not flag:
+				print (str(index)+' element is in close list : '+str(i.state))
+			return True
+	return False	
 
 def tile_up(state):
 	modified_state = state[:]
@@ -69,7 +87,7 @@ def display(Node):
 	print(str(s[3]) + "\t" + str(s[4]) + "\t" + str(s[5]))
 	print(str(s[6]) + "\t" + str(s[7]) + "\t" + str(s[8]))
 
-def dfs(start,goal,limit = 40):
+def dfs(start,goal,limit = 20):
 	open_list = []
 	close_list = []
 	open_list.append(create_node(start,None,None,1))
@@ -77,7 +95,8 @@ def dfs(start,goal,limit = 40):
 		if len(open_list) == 0:
 			return None
 		node = open_list.pop(0)
-		close_list.append(node)
+		if not member_c(node,close_list):
+			close_list.append(node)
 		if node.state == goal:
 			temp = node
 			moves = []
@@ -85,7 +104,7 @@ def dfs(start,goal,limit = 40):
 				if temp.depth == 1:
 					break
 				else:
-					display(temp)
+					#display(temp)
 					moves.insert(0,temp.move)
 					temp = temp.parent
 			return moves
@@ -104,7 +123,8 @@ def bfs(start,goal):
 			return None
 		else:
 			node = open_list.pop(0)
-			close_list.append(node)
+			if not member_c(node,close_list):
+				close_list.append(node)
 			if node.state == goal:
 				temp = node
 				moves = []
@@ -112,18 +132,20 @@ def bfs(start,goal):
 					if temp.depth == 1:
 						break
 					else:
-						display(temp)
+						#display(temp)
 						moves.insert(0,temp.move)
 						temp = temp.parent
 				return moves
 			open_list.extend(generate_children(node,open_list,close_list))
 
-def iterative_deepening(start,goal):
+def iterative_deepening(start,goal,limit = 20):
 	level = 1
 	while True:
 		Moves = dfs(start,goal,level)
 		if Moves == None:
 			level += 1
+			if level > limit:
+				return None
 		else:
 			return Moves
 
@@ -135,7 +157,8 @@ def hill_climbing(start,goal,limit = 40):
 		if len(open_list) == 0:
 			return None
 		node = open_list.pop(0)
-		close_list.append(node)
+		if not member_c(node,close_list):
+				close_list.append(node)
 		if node.state == goal:
 			temp = node
 			moves = []
@@ -143,7 +166,7 @@ def hill_climbing(start,goal,limit = 40):
 				if temp.depth == 1:
 					break
 				else:
-					display(temp)
+					#display(temp)
 					moves.insert(0,temp.move)
 					temp = temp.parent
 			return moves
@@ -161,7 +184,8 @@ def best_first(start,goal):
 		if len(open_list) == 0:
 			return None
 		node = open_list.pop(0)
-		close_list.append(node)
+		if not member_c(node,close_list):
+				close_list.append(node)
 		if node.state == goal:
 			temp = node
 			moves = []
@@ -169,7 +193,7 @@ def best_first(start,goal):
 				if temp.depth == 1:
 					break
 				else:
-					display(temp)
+					#display(temp)
 					moves.insert(0,temp.move)
 					temp = temp.parent
 			return moves
@@ -195,13 +219,16 @@ def index_pos(index):
 
 def main():
 	initial_state = [int(a) for a in raw_input('Give the initial state: ').split()] 
-	algorithm = eval(sys.argv[1])
-	Moves = algorithm(initial_state,goal_state)
-	if Moves == None:
-		print ('No path found')
-	else:
-		print ('Goal node has been found')
-		print (Moves)
+	algorithms = ['dfs','bfs','iterative_deepening','hill_climbing','best_first']
+	for al in algorithms:
+		algorithm = eval(al)
+		Moves = algorithm(initial_state,goal_state)
+		if Moves == None:
+			print ('No path found')
+		else:
+			print ('Goal node has been found by '+ al)
+			print (Moves)
+		raw_input('press any key to run next algorithm')
 
 if __name__ == '__main__':
 	main()
