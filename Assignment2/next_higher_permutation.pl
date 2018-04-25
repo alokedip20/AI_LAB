551edff7e7ac302df76cf32707d480ec867d8393
alokedip20 <alokengineer14@gmail.com>
2018-04-25 08:40:25 +0530

    updated

--- Assignment2/next_higher_permutation.pl
+++ Assignment2/next_higher_permutation.pl
@@ -1,54 +1,23 @@
-nhp(List,Result):-
-	digit(List,Initial),
-	nb_setval(diff,99999999),
-	nb_setval(no,-1),
-	not(nhp_acc(List,Initial,A,Result)),
-	nb_getval(no,X),
-	write("Digit ":X),
-	convert_list(X,Result).
-
-nhp_acc(List,Initial,A,Result):-
-	permutation(List,Temp),
-	digit(Temp,X),
-	T is X - Initial,
-	nb_getval(diff,Diff),
-	T > 0,
-		T < Diff,
-			nb_setval(diff,T),
-			nb_setval(no,X),
-			fail.
-	
-
-
-digit(List,R):-
-	my_length(List,Length),
-	digit_acc(List,Length,0,R),
-	!
-	.
-digit_acc(List,0,A,A).	
-digit_acc([Head|Rest],Length,A,R):-
-	T is Length - 1,	
-	power(10,T,X),
-	M is Head * X,
-	Temp is A + M,
-	N is Length - 1,
-	digit_acc(Rest,N,Temp,R).
-	
-
-power(X,Y,R):-
-	power_acc(X,Y,1,R).
-power_acc(X,0,A,A).
-power_acc(X,Y,A,R):-
-	T is A * X,
-	P is Y - 1,
-	power_acc(X,P,T,R).
-
-convert_list(Digit,Result):-
-	list_acc(Digit,[],Result).
-
-list_acc(0,A,A).
-list_acc(Digit,A,Result):-
-	X is mod(Digit,10),
-	T is div(Digit,10),
-	list_acc(T,[X|A],Result).
-	
+next_higher_permutation(L, L1) :-
+    higher_permutation(L, L1),
+    not(far_higher_permutation(L1, L)).
+        
+higher_permutation(L,L1) :-
+    permutation(L,L1),
+    higher(L1,L).
+        
+higher([H1|_],[H|_]) :- H1 > H ,!.        
+higher([H|T1],[H|T]) :- higher(T1,T).
+        
+far_higher_permutation(L1,L) :-
+    higher_permutation(L,L2),
+    higher(L1,L2).
+        
+permutation([],[]).
+permutation(L,[H|T]) :-
+    select(H,L,R),
+    permutation(R,T).
+        
+select(X,[X|R],R).
+select(X,[H|R],[H|R1]) :-
+    select(X,R,R1). 
